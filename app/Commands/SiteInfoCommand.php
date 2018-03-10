42a2796bfa3a2754f0e86aef6dafb8250a3e0833
Zorca <vs@zorca.org>
2018-03-10 17:41:59 +0300

    SiteInfo command added

--- app/Commands/SiteInfoCommand.php
+++ app/Commands/SiteInfoCommand.php
@@ -3,31 +3,34 @@
 use Illuminate\Console\Scheduling\Schedule;
 use Ox\Commands\Command;
 
-class SiteListCommand extends Command
+class SiteInfoCommand extends Command
 {
     /**
      * The signature of the command.
      *
      * @var string
      */
-    protected $signature = 'site:list';
+    protected $signature = 'site:info {site_name : Name of the site}';
 
     /**
      * The description of the command.
      *
      * @var string
      */
-    protected $description = 'List of created sites';
+    protected $description = 'Information about a site with giving name';
 
     /**
      * Execute the console command.
      */
     public function handle(): void
     {
-        $this->info("List of created sites");
-        $sites_dir = '/var/www';
-        $sites_dir_subdirs = glob($sites_dir.'/*', GLOB_ONLYDIR);
-        var_dump($sites_dir_subdirs);
+        $fs = app('filesystem.symfony');
+        $site_name = $this->argument('site_name');
+        $site_dir = '/var/www/'.$site_name;
+        if (!$fs->exists($site_dir)) {
+            $this->failure("Site directory: {$site_dir} not exists");
+        }
+        $this->info("Site: {$site_name}, directory: {$site_dir}");
     }
 
     /**
